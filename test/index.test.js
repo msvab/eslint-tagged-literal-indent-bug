ff26d9c75cad10493f96ea6595341d1387c06e17
Michal Šváb <michal@svab.net>
2019-08-20 10:09:05 +0200

    Add errors in comments

--- test/index.test.js
+++ test/index.test.js
@@ -5,6 +5,6 @@ describe('Indentation Bug', () => {
     a    | b
     ${1} | ${1}
   `('$a == $b', ({ a, b }) => {
-    expect(a).toBe(b);
-  });
+    expect(a).toBe(b); // error  Expected indentation of 2 spaces but found 4  indent
+  }); // error  Expected indentation of 0 spaces but found 2  indent
 });
